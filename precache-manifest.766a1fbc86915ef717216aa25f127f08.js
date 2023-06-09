c4b2f16ef3be441b3d370e0c85ab4f92a348b7dd
Ayshna12 <ayshnagoenka12@gmail.com>
2023-06-09 11:43:01 +0530

    Updates

---
+++ precache-manifest.766a1fbc86915ef717216aa25f127f08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b429755cff5325dac897",
+    "url": "/Conferences/static/css/main.c9330a41.chunk.css"
+  },
+  {
+    "revision": "b429755cff5325dac897",
+    "url": "/Conferences/static/js/main.b429755c.chunk.js"
+  },
+  {
+    "revision": "35ffad1a0530a19d1c0c",
+    "url": "/Conferences/static/js/1.35ffad1a.chunk.js"
+  },
+  {
+    "revision": "b6114eadb85e1e1ac3ec",
+    "url": "/Conferences/static/js/runtime~main.b6114ead.js"
+  },
+  {
+    "revision": "dab5fc377eb7213364118f5f8b711953",
+    "url": "/Conferences/index.html"
+  }
+];
